5e5c8aee1492d268fe9ae72a12bf1667f6b9514b
kaz <deadconvicess@gmail.com>
2025-07-21 15:16:09 -0700

    Vm1.py

--- Scripts you can add/Vm1.py
+++ Scripts you can add/Vm1.py
@@ -3,12 +3,14 @@
 
 
 # Simple VM Checker  
-def Vm():
-    import platform, os, uuid, psutil
-    users = [
-        "WDAGUtilityAccount", "sandbox", "user", "test", "JOHN-PC", "Malware", "vm",
-        "admin", "Administrator", "testuser", "analyst", "debug", "maltest", "malbox",
-        "lab", "research", "AVTest", "testvm", "vmuser", "winvm", "vmadmin", "test1",
+import os
+import uuid
+import psutil
+def vmchecker():
+    vm_usernames = {
+        "wdagutilityaccount", "sandbox", "user", "test", "john-pc", "malware", "vm",
+        "admin", "administrator", "testuser", "analyst", "debug", "maltest", "malbox",
+        "lab", "research", "avtest", "testvm", "vmuser", "winvm", "vmadmin", "test1",
         "qauser", "qa", "qadev", "forensics", "detonation", "honeypot", "dummy",
         "security", "analysis", "virtual", "vbox", "vmware", "hyperv", "win7test",
         "win10test", "vmlab", "vmtest", "pcuser", "sample", "malanalyzer", "viruslab",
@@ -23,7 +25,7 @@ def Vm():
         "tester1", "test_account", "remoteuser", "safeuser", "unknown", "unnamed", "analyzer",
         "protection", "protected", "analysisvm", "sandboxuser", "vmanalysis", "scanuser",
         "scantool", "sigscan", "artifact", "artifactuser", "controlled", "loggeduser",
-        "AVScanner", "AVEngine", "DYN_USER", "guest", "evaluser", "evalbox", "evalvm",
+        "avscanner", "avengine", "dyn_user", "guest", "evaluser", "evalbox", "evalvm",
         "cyberuser", "cyberlab", "cybervm", "root", "rootvm", "d3bug", "quarantine",
         "observed", "virmachine", "suspect", "containment", "fireeye", "crowdstrike",
         "mde", "sentinelone", "carbonblack", "vmcarbon", "infosec", "vmsec", "info_user",
@@ -33,12 +35,12 @@ def Vm():
         "defaultuser", "pc-lab", "winlabvm", "win10sandbox", "sandboxvm", "detectionuser",
         "witness", "eval", "infosectest", "valvm", "dlab", "nodevm", "clouduse", "awsuser",
         "azureuser", "gcpvm", "vboxuser", "defaultuser0", "user001", "vmlocal", "corpvm",
-        "forensic", "avengine", "AVUser", "avbox", "malresearch", "scanvm", "monitor",
+        "forensic", "avengine", "avuser", "avbox", "malresearch", "scanvm", "monitor",
         "vmobserver", "surveyor", "surveil", "investig8", "dmzvm", "testenv", "tstuser",
         "securityeng", "analyzevm", "sysmon", "loguser", "huntvm", "hunter", "watcher",
         "sensor", "inteluser", "fwuser", "winsec", "secuser", "restricted", "robox", "detectionvm"
-    ]
-    macs = [
+    }
+    vm_mac_prefixes = {
         "00:05:69", "00:0C:29", "00:1C:14", "00:50:56", "00:03:FF", "00:1C:42", "00:0F:4B", "00:16:3E",
         "00:15:5D", "00:21:F6", "08:00:27", "52:54:00", "00:14:4F", "00:25:90", "00:0D:3A", "00:1F:29",
         "00:18:51", "00:50:43", "00:0C:EC", "00:1B:21", "00:1B:24", "00:13:74", "00:25:AE", "00:05:9A",
@@ -48,16 +50,18 @@ def Vm():
         "00:1F:C6", "00:0A:E4", "00:50:58", "00:1F:16", "00:1E:EC", "00:25:96", "00:24:81", "00:1F:90",
         "00:1A:A0", "00:50:BE", "00:50:C2", "00:1E:90", "00:21:CC", "00:50:AE", "00:30:5A", "00:17:FA",
         "00:1D:09", "00:50:F2", "00:1E:68"
-    ]
+    }
     username = os.getenv("USERNAME", "").lower()
-    if any(user.lower() in username for vm_user in users):
+    if username in vm_usernames:
         return True
-    mac = ':'.join(['{:02x}'.format((uuid.getnode() >> ele) & 0xff)
-                    for ele in range(0, 8*6, 8)][::-1]).upper()
-    if any(mac.startswith(prefix) for prefix in macs):
+    mac_num = uuid.getnode()
+    mac_addr = ':'.join(f"{(mac_num >> ele) & 0xff:02X}" for ele in range(40, -8, -8))
+    if any(mac_addr.startswith(prefix) for prefix in vm_mac_prefixes):
         return True
-    if psutil.virtual_memory().total < 2 * 1024 * 1024 * 1024:
+    if psutil.virtual_memory().total < 2 * 1024 ** 3:
         return True
-    if psutil.cpu_count(logical=False) < 2:
+    if psutil.cpu_count(logical=False) is not None and psutil.cpu_count(logical=False) < 2:
         return True
+
     return False
+
